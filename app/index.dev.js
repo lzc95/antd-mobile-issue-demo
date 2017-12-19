e8f5dc18251adfa5f1ac58c75445ca2c379d3bfd
yStOpHOlsiStriaN <you@example.com>
2017-12-19 15:15:04 +0800

    :white_check_mark: Update ok

--- app/index.dev.js
+++ app/index.dev.js
@@ -1,22 +1,57 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Button, Toast } from 'antd-mobile';
-import { Button as PCButton } from 'antd';
+// import { Button, Toast } from 'antd-mobile';
+import { Button, Modal } from 'antd';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import styled, { injectGlobal } from 'styled-components';
+import 'antd/lib/style/v2-compatible-reset';
 
 function failToast() {
-  Toast.fail('Load failed !!!', 1);
+    Toast.fail('Load failed !!!', 1);
 }
 
 class App extends React.Component {
-
+    state = { visible: false }
+    showModal = () => {
+        this.setState({
+            visible: true,
+        });
+    }
+    handleOk = (e) => {
+        this.setState({
+            visible: false,
+        });
+    }
+    handleCancel = (e) => {
+        this.setState({
+            visible: false,
+        });
+    }
     render() {
         return (
             <div>
-                <Button>Mobile Button</Button>
-                <PCButton>PC Button</PCButton>
-                <Button onClick={failToast}>text only</Button>
+                <ul>
+                    <li>dfsdf</li>
+                    <li>dfsdf</li>
+                    <li>dfsdf</li>
+                    <li>dfsdf</li>
+                    <li>dfsdf</li>
+                </ul>
+
+                {/*<Button>Mobile Button</Button>*/}
+                {/*<Button onClick={failToast}>Toast fail</Button>*/}
+
+                <Button type="primary" onClick={this.showModal}>Open Modal</Button>
+                <Modal
+                  title="Basic Modal"
+                  visible={this.state.visible}
+                  onOk={this.handleOk}
+                  onCancel={this.handleCancel}
+                >
+                  <p>Some contents...</p>
+                  <p>Some contents...</p>
+                  <p>Some contents...</p>
+                </Modal>
             </div>
         );
     }
